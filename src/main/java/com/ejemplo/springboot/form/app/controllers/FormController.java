daead4e47555c5633da109267a94a64bc8288d3f
steeltx <realsteel.gnu@gmail.com>
2022-03-16 00:45:14 -0600

    Recibir datos de formulario

--- src/main/java/com/ejemplo/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/ejemplo/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ejemplo.springboot.form.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario de usuarios");
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	public String procesar(Model model, @RequestParam String username, @RequestParam String password,
+			@RequestParam String email) {
+
+		Usuario usuario = new Usuario();
+		usuario.setUsername(username);
+		usuario.setEmail(email);
+		usuario.setPassword(password);
+
+		model.addAttribute("titulo", "Resultado del form");
+		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
 
